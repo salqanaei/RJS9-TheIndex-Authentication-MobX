e6efefe9c0d3c80e7e70cf140fb04a10be084abb
Sulaiman AlQanaei <salqanaei@Sulaimans-MacBook-Air-2.local>
2019-08-30 12:18:26 +0300

    Final

--- src/AddAuthorCard.js
+++ src/AddAuthorCard.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Modal from "react-responsive-modal";
-
+import authStore from "./stores/authStore";
 import AuthorForm from "./forms/AuthorForm";
 
 class AddAuthorCard extends Component {
@@ -14,29 +14,31 @@ class AddAuthorCard extends Component {
 
   render() {
     const { open } = this.state;
-    return (
-      <div className="col-lg-4 col-md-6 col-12">
-        <div>
-          <Modal open={open} onClose={this.onCloseModal} center>
-            <AuthorForm closeModal={this.onCloseModal} />
-          </Modal>
-        </div>
-        <div className="card" onClick={this.onOpenModal}>
-          <div className="image">
-            <img
-              className="card-img-top img-fluid"
-              src="https://mbtskoudsalg.com/images/a-plus-png-2.png"
-              alt="+"
-            />
+    {
+      return (
+        <div className="col-lg-4 col-md-6 col-12">
+          <div>
+            <Modal open={open} onClose={this.onCloseModal} center>
+              <AuthorForm closeModal={this.onCloseModal} />
+            </Modal>
           </div>
-          <div className="card-body">
-            <h5 className="card-title">
-              <span>Add Author</span>
-            </h5>
+          <div className="card" onClick={this.onOpenModal}>
+            <div className="image">
+              <img
+                className="card-img-top img-fluid"
+                src="https://mbtskoudsalg.com/images/a-plus-png-2.png"
+                alt="+"
+              />
+            </div>
+            <div className="card-body">
+              <h5 className="card-title">
+                <span>Add Author</span>
+              </h5>
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
